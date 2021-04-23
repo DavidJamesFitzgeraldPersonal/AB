3d94795ccb71165112179f475a53cfa7b52ba3c3
DavidFitzgerald <David.fitzgerald@live.co.uk>
2021-04-23 11:52:50 +0100

    Initial commit.

---
+++ UniProject/UniProject/UniProject/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UniProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+
+            bool isFingerprintAvailable = await CrossFingerprint.Current.IsAvailableAsync(false);
+            if (!isFingerprintAvailable)
+            {
+                await DisplayAlert("Error",
+                    "Biometric authentication is not available or is not configured.", "OK");
+                return;
+            }
+
+            AuthenticationRequestConfiguration conf =
+                new AuthenticationRequestConfiguration("Authentication",
+                "Authenticate access to your personal data");
+
+            var authResult = await CrossFingerprint.Current.AuthenticateAsync(conf);
+            if (authResult.Authenticated)
+            {
+                //Success  
+                await DisplayAlert("Success", "Authentication succeeded", "OK");
+                await Navigation.PushAsync(new ControlPage());
+            }
+            else
+            {
+                await DisplayAlert("Error", "Authentication failed", "OK");
+            }
+        }
+    }
+}
